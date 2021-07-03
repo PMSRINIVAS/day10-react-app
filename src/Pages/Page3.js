a9097e2668f87b2ea70b2d727056158a9d4f38ed
PMSRINIVAS <killermaddy55@gmail.com>
2021-07-04 01:07:22 +0530

    Counter increment and decrement like last proj

--- src/Pages/Page3.js
+++ src/Pages/Page3.js
@@ -1,13 +1,24 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Page3() {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state);
+
+  const increment = () => {
+    //Logic is not given here. its in store.js
+    dispatch({ type: "INCREMENT" });
+  };
+
   return (
     <div>
       <h1>Page3</h1>
-
-      <div className="alert alert-secondary">{state.counter}</div>
-
+      <div className="alert alert-secondary">{state.counter}</div>\
+      <input
+        type="button"
+        value="Increment"
+        onClick={increment}
+        className="btn btn-secondary w-100"
+      />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, neque
         sapiente! Quibusdam aliquid reiciendis totam officiis quidem voluptate,
